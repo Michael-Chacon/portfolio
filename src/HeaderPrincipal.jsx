4a5963e993a6cd1348a485a0ab66c8b91410fee0
Michael-Chacon <michaelchacon02@gmail.com>
2024-11-07 18:58:20 -0500

    feat: :sparkles: Creado componente principal con estilos asignados
    Se creó el componente principal del portafolio y se aplicaron estilos iniciales para su estructura y presentación.

---
+++ src/HeaderPrincipal.jsx
@@ -0,0 +1,12 @@
+export function HeaderPrincipal(){
+    return (
+        <header class="principal__header">
+        <div class="principal__flechas">
+          <i class='bx bx-chevron-left'></i>
+          <i class='bx bx-chevron-right' style="color: gray;"></i>
+        </div>
+        <p class="principal__titulo"><i class='bx bxs-hand-right'></i> Proyectos</p>
+        <i class='principal__header--modo bx bx-moon' ></i>
+      </header>
+    )
+}
---
+++ src/Secciones.jsx
@@ -0,0 +1,9 @@
+export function Secciones(){
+    return (
+        <section class="principal__secciones">
+        <div class="contenido">
+        
+        </div>
+      </section>
+    )
+}
