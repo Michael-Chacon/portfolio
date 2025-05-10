5c44bef8ed5bbde00cf85a4bfbd8419572e28fe4
Michael-Chacon <michaelchacon02@gmail.com>
2025-05-09 20:26:39 -0500

    feat: 🌐 Enlaces externos a LinkedIn y GitHub en el menú principal
    Se añadieron dos nuevas opciones al menú principal que redirigen a los perfiles de LinkedIn y GitHub, permitiendo a los usuarios acceder fácilmente a información profesional y proyectos del portafolio.

--- src/ContainerPrincipal.jsx
+++ src/ContainerPrincipal.jsx
@@ -14,20 +14,26 @@ export function ContainerPrincipal() {
     <>
       <section className="container">
         <aside className="menu">
-          <img className="menu__logo" src="logo.png" alt="logo portfolio Alexis" />
-          <ul className="menu__opciones">
-            <NavLink to="/perfil" className="menu__secciones" data-tooltip-id="perfil" data-tooltip-content="Perfil"><i className='bx bxs-user-rectangle'></i></NavLink>
-            <Tooltip id='perfil' className='tooltip-personalizado' place='right' />
-            <NavLink to="/skills" className="menu__secciones" data-tooltip-id="skills" data-tooltip-content=" Skills"><i className='bx bx-check-double'></i></NavLink>
-            <Tooltip id='skills' className='tooltip-personalizado' place='right' />
-            <NavLink to="/proyectos" className="menu__secciones" data-tooltip-id="proyectos" data-tooltip-content="Proyectos"><i className='bx bx-folder-open'></i></NavLink>
-            <Tooltip id='proyectos' className='tooltip-personalizado' />
-            <NavLink to="/" className="menu__secciones" data-tooltip-id="otros" data-tooltip-content="Otros"><i className='bx bx-customize'></i></NavLink>
-            <Tooltip id='otros' className='tooltip-personalizado' place='right' />
-            <NavLink to="/" className="menu__secciones" data-tooltip-id="otros" data-tooltip-content="Otros"><i className='bx bxs-contact'></i></NavLink>
-            <Tooltip id='otros' className='tooltip-personalizado' place='right' />
-            <NavLink to="/" className="menu__secciones" data-tooltip-id="otros" data-tooltip-content="Otros"><i className='bx bx-heart'></i></NavLink>
-            <Tooltip id='otros' className='tooltip-personalizado' place='right' />
+          <div>
+            <img className="menu__logo" src="logo.png" alt="logo portfolio Alexis" />
+            <ul className="menu__opciones">
+              <NavLink to="/perfil" className="menu__secciones" data-tooltip-id="perfil" data-tooltip-content="Perfil"><i className='bx bxs-user-rectangle'></i></NavLink>
+              <Tooltip id='perfil' className='tooltip-personalizado' place='right' />
+              <NavLink to="/skills" className="menu__secciones" data-tooltip-id="skills" data-tooltip-content=" Skills"><i className='bx bx-check-double'></i></NavLink>
+              <Tooltip id='skills' className='tooltip-personalizado' place='right' />
+              <NavLink to="/proyectos" className="menu__secciones" data-tooltip-id="proyectos" data-tooltip-content="Proyectos"><i className='bx bx-folder-open'></i></NavLink>
+              <Tooltip id='proyectos' className='tooltip-personalizado' />
+              <NavLink to="/" className="menu__secciones" data-tooltip-id="certificados" data-tooltip-content="Certificados"><i class='bx bx-certification'></i></NavLink>
+              <Tooltip id='certificados' className='tooltip-personalizado' place='right' />
+              <NavLink to="/" className="menu__secciones" data-tooltip-id="meGusta" data-tooltip-content="Cosas favoritas"><i className='bx bx-heart'></i></NavLink>
+              <Tooltip id='meGusta' className='tooltip-personalizado' place='right' />
+            </ul>
+          </div>
+          <ul className='menu__opciones redes'>
+            <a href="https://www.linkedin.com/in/michael-alexis-chac%C3%B3n-mar%C3%ADn/" target='_black' className="menu__secciones" data-tooltip-id="linkedin" data-tooltip-content="Linkedin"><i class='bx bxl-linkedin-square'></i></a>
+            <Tooltip id='linkedin' className='tooltip-personalizado' place='right' />
+            <a href="https://github.com/Michael-chacon" target='_black' className="menu__secciones" data-tooltip-id="github" data-tooltip-content="Github"><i class='bx bxl-github'></i></a>
+            <Tooltip id='github' className='tooltip-personalizado' place='right' />
           </ul>
         </aside>
         <main className="principal">
