5a383a73fdbd13fcb83b136f2ce4e5297027c541
Michael-Chacon <michaelchacon02@gmail.com>
2025-05-09 19:50:39 -0500

    feat: 💬 Tooltips en el menú con react-tooltip y corrección en nombre de sección
    Se integró la librería react-tooltip para mostrar descripciones breves al pasar el cursor sobre las opciones del menú. Además, se solucionó un problema que recortaba incorrectamente una letra del nombre de la sección actual utilizando un operador ternario para manejar adecuadamente el formato de la ruta.

--- src/ContainerPrincipal.jsx
+++ src/ContainerPrincipal.jsx
@@ -1,13 +1,14 @@
 import { NavLink, Outlet, useLocation } from 'react-router-dom'
-import "./ContainerPrincipal.css"
 import { Navegacion } from './utils/Navegacion'
+import { Tooltip } from 'react-tooltip';
+import "./ContainerPrincipal.css"
 
 export function ContainerPrincipal() {
   const location = useLocation()
 
   // Obtener y limpiar el nombre de la ruta actual para mostrarlo en el header
   const rutaActual = location.pathname.replace('/', '')
-  const nombreRuta = rutaActual.charAt(0).toUpperCase() + rutaActual.slice(1, rutaActual.indexOf('/')) || 'Perfil'
+  const nombreRuta = rutaActual.includes('/') ? rutaActual.charAt(0).toUpperCase() + rutaActual.slice(1, rutaActual.indexOf('/')) || 'Perfil' : rutaActual.charAt(0).toUpperCase() + rutaActual.slice(1) || 'Perfil'
 
   return (
     <>
@@ -15,21 +16,23 @@ export function ContainerPrincipal() {
         <aside className="menu">
           <img className="menu__logo" src="logo.png" alt="logo portfolio Alexis" />
           <ul className="menu__opciones">
-            <NavLink to="/perfil" className="menu__secciones" title="Seccion tal"><i className='bx bxs-user-rectangle'></i></NavLink>
-            <NavLink to="/skills" className="menu__secciones" title="Seccion tal"><i className='bx bx-check-double'></i></NavLink>
-            <NavLink to="/proyectos" className="menu__secciones" title="Seccion tal"><i className='bx bx-folder-open'></i></NavLink>
-            <NavLink to="/" className="menu__secciones" title="Seccion tal"><i className='bx bx-customize'></i></NavLink>
-            <NavLink to="/" className="menu__secciones" title="Seccion tal"><i className='bx bxs-contact'></i></NavLink>
-            <NavLink to="/" className="menu__secciones" title="Seccion tal"><i className='bx bx-heart'></i></NavLink>
+            <NavLink to="/perfil" className="menu__secciones" data-tooltip-id="perfil" data-tooltip-content="Perfil"><i className='bx bxs-user-rectangle'></i></NavLink>
+            <Tooltip id='perfil' className='tooltip-personalizado' place='right' />
+            <NavLink to="/skills" className="menu__secciones" data-tooltip-id="skills" data-tooltip-content=" Skills"><i className='bx bx-check-double'></i></NavLink>
+            <Tooltip id='skills' className='tooltip-personalizado' place='right' />
+            <NavLink to="/proyectos" className="menu__secciones" data-tooltip-id="proyectos" data-tooltip-content="Proyectos"><i className='bx bx-folder-open'></i></NavLink>
+            <Tooltip id='proyectos' className='tooltip-personalizado' />
+            <NavLink to="/" className="menu__secciones" data-tooltip-id="otros" data-tooltip-content="Otros"><i className='bx bx-customize'></i></NavLink>
+            <Tooltip id='otros' className='tooltip-personalizado' place='right' />
+            <NavLink to="/" className="menu__secciones" data-tooltip-id="otros" data-tooltip-content="Otros"><i className='bx bxs-contact'></i></NavLink>
+            <Tooltip id='otros' className='tooltip-personalizado' place='right' />
+            <NavLink to="/" className="menu__secciones" data-tooltip-id="otros" data-tooltip-content="Otros"><i className='bx bx-heart'></i></NavLink>
+            <Tooltip id='otros' className='tooltip-personalizado' place='right' />
           </ul>
         </aside>
         <main className="principal">
           <header className="principal__header">
-            {/* <div className="principal__flechas">
-              <i className='bx bx-chevron-left'></i>
-              <i className='bx bx-chevron-right' style={{ color: 'gray' }}></i>
-            </div> */}
-            <Navegacion/>
+            <Navegacion />
             <p className="principal__titulo"><i className='bx bxs-hand-right'></i> {nombreRuta}</p>
             <i className='principal__header--modo bx bx-moon' ></i>
           </header>
