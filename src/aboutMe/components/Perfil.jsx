e047d2876c6ff546e8e1567be7a5f94722cca758
Alexis <michaelchacon02@gmail.com>
2025-10-07 17:28:43 -0500

    fix: Se quito  el boton para descargar la hoja de vida

--- src/aboutMe/components/Perfil.jsx
+++ src/aboutMe/components/Perfil.jsx
@@ -12,7 +12,7 @@ export function Perfil() {
             Soy desarrollador Java especializado en APIs RESTful con Spring Boot e Hibernate. Diseño y gestiono bases de datos relacionales con MySQL, y construyo arquitecturas de microservicios usando Spring Boot, Eureka y Docker. Aunque mi pasión es el backend, también desarrollo interfaces web con HTML, CSS, JavaScript y React, y utilizo Git para el control de versiones. Estoy enfocado en el desarrollo de código de calidad y disfruto compartir conocimientos y apoyar a otros en su aprendizaje.
 
           </p>
-          <button class="perfil__btn__cv">Hoja de vida <i class='bx bxs-download'></i></button>
+          {/* <button class="perfil__btn__cv">Hoja de vida <i class='bx bxs-download'></i></button> */}
         </section>
         <section class="perfil__marco__foto">
         </section>
